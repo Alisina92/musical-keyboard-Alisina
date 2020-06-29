b9acfbe3a868d254894120b82af7e0d9d0ebf179
[Alisina92] <asb92.jobs@gmail.com>
2020-06-29 15:46:33 +0100

    1-Title Component added to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Title from './Title'
+import Keyboard from'./Keyboard';
+import Options from './Options';
 import './App.css';
 
-function App() {
+const App=()=> {
+   /*Component added to App:
+    <Title/>
+    <Keyboard/>
+    <Options/>
+   */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Title/>
+    <Keyboard/>
+    <Options/>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title =()=>{
+     /*Title component added*/
+    return(<div>
+     <h1>Alisina's Music-Box</h1>
+          </div>)
+}
+
+export default Title;
